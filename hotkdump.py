2c765f1538eadba5f7c314ecf980e58b97081097
Nikhil Kshirsagar <nikhil.kshirsagar@canonical.com>
2023-03-02 11:59:22 +0000

    download the dbgsym using apt-get, all debugs left in

--- hotkdump.py
+++ hotkdump.py
@@ -4,7 +4,6 @@ import os
 import sys
 import subprocess
 import logging
-
 from subprocess import run
 
 #am sure will need all this later
@@ -71,13 +70,65 @@ class hotkdump:
         return str(output)
 
     def download_vmlinux(self, kernel_version: str):
+        # see https://wiki.ubuntu.com/Debug%20Symbol%20Packages
         print("Downloading vmcore for kernel", kernel_version)
         logging.info("Downloading vmcore for kernel %s",kernel_version)
-        # stub, this returns the filename it downloads and keeps in cwd
-        # hardcode for now
-
-        # see https://wiki.ubuntu.com/Debug%20Symbol%20Packages
-        return "vmlinux-5.15.0-52-generic"
+        cmd = "apt-get "
+        kernel_version = kernel_version.rstrip()
+        check_cwd = "./vmlinux-" + kernel_version
+        if os.path.exists(check_cwd):
+            print("% file already exists in cwd ",check_cwd)
+            return "vmlinux-" + kernel_version
+        elif os.path.exists("/lib/debug/boot/vmlinux-" + kernel_version):
+            # this stuff really needs its own function so can be reused below
+            print("vmlinux for % found in /lib/debug/boot", kernel_version)
+            #move the debug file into cwd
+            cmd = "cp "
+            args = "/lib/debug/boot/" + "vmlinux-" + kernel_version 
+            fullargs = args + " ."
+            fullcmd = cmd + fullargs
+            print(fullcmd)
+            print("looking for", args)
+            if os.path.exists(args.rstrip()):
+                print("found")
+                print(cmd+args)
+                print("doing cp now")
+                print(fullcmd)
+                output = self.execute_cmd(cmd,fullargs,0)
+                if output == 0:
+                    print("output 0")
+                    return "vmlinux-" + kernel_version
+                else:
+                    print("output of cp not 0")
+            else:
+                print("not exists??")
+                return ""
+            return "vmlinux-" + kernel_version
+        else:
+            # need to install the dbgsym
+            args = "install -y linux-image-" + kernel_version + "-dbgsym"
+            print(cmd + args)
+            print("sending that cmd off for execution")
+            output = self.execute_cmd(cmd, args,0)
+            if output == 0:
+                #move the debug file into cwd
+                cmd = "cp "
+                args = "/lib/debug/boot/" + "vmlinux-" + kernel_version 
+                fullargs = args + " ."
+                fullcmd = cmd + fullargs
+                print(fullcmd)
+                if os.path.exists(args):
+                    print("doing cp now")
+                    print(cmd+args)
+                    output = self.execute_cmd(cmd,fullargs,0)
+                    if output == 0:
+                        print("output 0")
+                        return "vmlinux-" + kernel_version
+                    else:
+                        print("output of cp not 0")
+            else:
+                print("output of apt-get install not 0")
+                return ""
         
 def main():
     hotk = hotkdump()
@@ -86,30 +137,39 @@ def main():
     kernel_version = hotk.get_kernel_version(str(hotk.vmcore))
     logging.info("%s is kernel_version",kernel_version)
     vmlinux = hotk.download_vmlinux(kernel_version)
+    if vmlinux == "": 
+        print("got empty vmlinux")
+        ## goto out? :-X
+    else:
+        # assuming vmcore exists in cwd for now
+        # can figure later how to download it from files.canonical given a 
+        # casenum (for athena) and the name or full files.canonical path of the vmcore
+        dump = hotk.vmcore
+        print(dump)
+        print("is dump\n")
+        print(vmlinux)
+        print("is vmlinux")
+        args = str(dump) + " " + str(vmlinux)
+        print("\n and args are")
+        print(args)
+        logging.info("args to main are ...")
+        logging.info(args)
 
-    # taking from args for now, assuming vmcore exists in cwd for now
-    # can figure later how to download it from files.canonical given a 
-    # casenum (for athena) and the name or full files.canonical path of the vmcore
-    dump = hotk.vmcore
-    args = str(dump) + " " + str(vmlinux)
-    logging.info("args to main are ...")
-    logging.info(args)
-
-    # todo move cmd to a class variable
-    # remove the -s if you want to see console output 
-    # of what crash is upto, in case there's a failure
-    cmd = "./crash -s "
-    print("Loading vmcore into crash.. please wait..")
-    output = hotk.execute_cmd(cmd,args,0)
-    file_crashrc = open('/home/hotkdump/.crashrc', 'r')
-    all_crashrc_commands = file_crashrc.readlines()
-    print("Collected output of.. \n")
-    for line in all_crashrc_commands:
-        if 'echo' not in line and 'quit' not in line:
-            line_to_print = line.split(">")[0]
-            print(line_to_print)
-    print("\nSee hotkdump.log for logs")
-    print("See hotkdump.out for output")
+        # todo move cmd to a class variable
+        # remove the -s if you want to see console output 
+        # of what crash is upto, in case there's a failure
+        cmd = "./crash -s "
+        print("Loading vmcore into crash.. please wait..")
+        output = hotk.execute_cmd(cmd,args,0)
+        file_crashrc = open('/home/hotkdump/.crashrc', 'r')
+        all_crashrc_commands = file_crashrc.readlines()
+        print("Collected output of.. \n")
+        for line in all_crashrc_commands:
+            if 'echo' not in line and 'quit' not in line:
+                line_to_print = line.split(">")[0]
+                print(line_to_print)
+        print("\nSee hotkdump.log for logs")
+        print("See hotkdump.out for output")
 
 if __name__=="__main__":
     main()
