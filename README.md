52bbcf30d7fdaecf6ac40ccd8196f75eb65933df
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2023-04-03 16:13:01 +0300

    ddeb-cache: implement retention policy & log level env variable
    implemented a retention policy that supports 3 criterias:
    - total folder size
    - count of ddebs
    - age of the ddeb file
    
    added support for reading log level from HOTKDUMP_LOGLEVEL env variable
    added initial set of unit tests for hotkdump
    
    Fixes #7
    
    Signed-off-by: Mustafa Kemal Gilor <mustafa.gilor@canonical.com>

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ hotkdump is a tool that takes a SF casenum and a downloaded vmcore as inputs and
 containing information extracted from the vmcore, which helps understand root causes of kernel panics or hangs. 
 
 See how_to_run_and_output for a basic manual run. 
+
+## Running tests
+
+```sh
+    python3 -m pytest
+```
--- hotkdump.py
+++ hotkdump.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import re
 import subprocess
 import logging
 import sys
@@ -9,6 +10,7 @@ import shutil
 import time
 import textwrap
 from datetime import datetime
+import contextlib
 
 try:
     from ubuntutools.pullpkg import PullPkg
@@ -16,10 +18,6 @@ try:
 except ModuleNotFoundError:
     raise ModuleNotFoundError("\n\n`hotkdump` needs ubuntu.pullpkg to function.\n"
                               "Install it via `sudo apt install ubuntu-dev-tools`")
-
-# am sure will need all this later
-from typing import Dict, List, Tuple, Union
-
 
 """
 TODOS:
@@ -33,6 +31,29 @@ and for which case, for automatically updating the case
 
 """
 
+def pretty_size(bytes):
+    """Get human-readable file sizes.
+    simplified version of https://pypi.python.org/pypi/hurry.filesize/
+    """
+    UNITS_MAPPING = [
+        (1<<50, 'PB'),
+        (1<<40, 'TB'),
+        (1<<30, 'GB'),
+        (1<<20, 'MB'),
+        (1<<10, 'KB'),
+        (1, ('byte', 'bytes')),
+    ]
+    for factor, suffix in UNITS_MAPPING:
+        if bytes < factor:
+            continue
+        amount = bytes / factor
+        if isinstance(suffix, tuple):
+            singular, multiple = suffix
+            if amount == 1:
+                suffix = singular
+            else:
+                suffix = multiple
+        return f"{amount:.2f} {suffix}"
 
 class ExceptionWithLog(Exception):
 
@@ -81,6 +102,7 @@ class kdump_file_header(object):
             self.machine = self.readcstr(fd)
             self.domain = self.readcstr(fd)
             self.normalized_version = self.version.split("-")[0].lstrip("#")
+            logging.debug(f"kdump_hdr: {str(self)}")
 
     @staticmethod
     def seek_to_first_non_nul(f):
@@ -109,7 +131,7 @@ class kdump_file_header(object):
         buf = str()
         while True:
             b = f.read(1)
-            if (b is None) or (b == b'\x00'):
+            if (b == b'') or (b == b'\x00'):
                 kdump_file_header.seek_to_first_non_nul(f)
                 return str(''.join(buf))
             else:
@@ -118,44 +140,60 @@ class kdump_file_header(object):
     def __str__(self) -> str:
 
         return " | ".join(
-                # Get all attributes, filter out the built-in ones
-                # and stringify the rest in "name:value" format
-                [f" {v}:{str(getattr(self, v))}" for v in filter(
-                    lambda x: not x.startswith("__"), dir(self))]
-            )
+            # Get all attributes, filter out the built-in ones
+            # and stringify the rest in "name:value" format
+            [f" {v}:{str(getattr(self, v))}" for v in filter(
+                lambda x: not x.startswith("__") and not callable(getattr(self, x)), dir(self))]
+        )
 
 
 default_output_file = "hotkdump.out"
 default_log_file = "hotkdump.log"
+default_ddebs_path = "/tmp/hotkdump/ddebs"
 
 
 class hotkdump:
 
-    def __init__(self, case_number, vmcore_file, output_file_path=default_output_file, log_file_path=default_log_file):
+    def __init__(self, case_number, vmcore_file, output_file_path=default_output_file, log_file_path=default_log_file, ddebs_path=default_ddebs_path):
         self.output_file = output_file_path
         self.log_file = log_file_path
         self.case_number = case_number
         self.vmcore_file = vmcore_file
+        self.ddebs_path = ddebs_path
         self.crash_executable = self.find_crash_executable()
+
+
+        self.ddeb_retention_enabled = False
+        self.ddeb_retention_size_high_wm_bytes = None # (1<<30) * 2 # 2 GiB
+        self.ddeb_retention_size_low_wm_bytes = None # (1<<30) * 4 # 4 GiB
+        self.ddeb_retention_max_age_secs = None # 86400 * 15 # 15 days
+        self.ddeb_retention_max_ddeb_count = None # 2
+
+        if (self.ddeb_retention_size_high_wm_bytes and self.ddeb_retention_size_low_wm_bytes) and (self.ddeb_retention_size_high_wm_bytes < self.ddeb_retention_size_low_wm_bytes):
+            raise ExceptionWithLog("ddeb high watermark cannot be less than low watermark!")
+
         self.initialize_logging()
 
         logging.info(
             f"initializing hotkdump, SF#{self.case_number}, vmcore: {self.vmcore_file}")
 
+        self.touch_file(self.output_file)
         tstamp_now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         vmcore_filename  = self.vmcore_file.rsplit('/', 1)[-1]
         with open(self.output_file, "w") as outfile:
-            outfile.write("Timestamp: " + tstamp_now+"\n")
-            outfile.write("Processing " + vmcore_filename+"\n")
+            outfile.write("{tstamp_now}: processing {vmcore_filename} (SF# {self.case_number})\n")
 
         self.kdump_header = kdump_file_header(self.vmcore_file)
 
         logging.info(
             f"kernel version: {self.kdump_header.release}")
         self.temp_working_dir = tempfile.TemporaryDirectory()
-        logging.info(
+        logging.debug(
             f"created {self.temp_working_dir.name} temporary directory for the intermediary files")
         self.commands_file_path = self.write_crash_commands_file()
+
+        # Create the ddeb path if not exists
+        os.makedirs(default_ddebs_path, exist_ok=True)
 
     def get_architecture(self):
         if self.kdump_header.machine == "x86_64":
@@ -187,17 +225,25 @@ class hotkdump:
         """
         self.logger = logging.getLogger()
         file_logger = logging.FileHandler(filename=self.log_file)
-        file_logger.setLevel(logging.INFO)
         console_logger = logging.StreamHandler(sys.stdout)
-        console_logger.setLevel(logging.DEBUG)
-        self.logger.addHandler(file_logger)
-        self.logger.addHandler(console_logger)
-        self.logger.setLevel(logging.INFO)
+        # Allow log level overrides from environment
+        level = os.environ.get('HOTKDUMP_LOGLEVEL', 'INFO').upper()
+
+        for logger in [file_logger, console_logger, self.logger]:
+            logger.setLevel(level)
+
+        for logger in [file_logger, console_logger]:
+            self.logger.addHandler(logger)
 
         # Only display error messages and the download status from ubuntutools
         for handler in ubuntutools_GetLogger().handlers:
             handler.addFilter(
                 lambda r: "Downloading" in r.msg or r.levelno >= logging.ERROR)
+
+    @staticmethod
+    def touch_file(fname):
+        with open(fname, "w"):
+            pass
 
     def write_crash_commands_file(self):
         """
@@ -227,7 +273,6 @@ class hotkdump:
             #   after a write() of the data, it must reflect that write(), even
             #   if the calls are made by different processes."
             commands_file_content = fr"""
-            !echo "timestamp: {time.time()}"
             !echo "---------------------------------------" >> {self.output_file}
             !echo "Output of 'sys'" >> {self.output_file}
             !echo "---------------------------------------" >> {self.output_file}
@@ -299,23 +344,37 @@ class hotkdump:
         p.wait()
         return p
 
+    @contextlib.contextmanager
+    def switch_cwd(self, wd):
+        curdir = os.getcwd()
+        try:
+            os.chdir(wd)
+            yield
+        finally: os.chdir(curdir)
+
     @staticmethod
     def strip_release_variant_tags(str):
         # see: https://ubuntu.com/kernel/variants#version-specific-kernels
-        tags = ["generic", "lowlatency", "generic-hwe",
-                "lowlatency-hwe", "kvm", "aws", "azure", "azure-fde",
-                "gcp", "gke", "snapdragon", "raspi2"]
-        version_specific_tags = [
-            "generic-hwe-{}", "generic-hwe-{}", "lowlatency-hwe-{}", "lowlatency-hwe-{}"]
+        tags = sorted(["generic", "lowlatency", "generic-hwe",
+                       "lowlatency-hwe", "kvm", "aws", "azure", "azure-fde",
+                       "gcp", "gke", "snapdragon", "raspi2"], key=len, reverse=True)
+        version_specific_tags = sorted([
+            "generic-hwe-{}", "generic-hwe-{}", "lowlatency-hwe-{}", "lowlatency-hwe-{}"], key=len, reverse=True)
         versions = ["16.04", "18.04", "20.04", "22.04", "24.04"]
+
+        for vtag in version_specific_tags:
+            for version in versions:
+                str = str.replace("-" + vtag.format(version) + '-edge', '')
+                str = str.replace("-" + vtag.format(version), '')
 
         for tag in tags:
             str = str.replace(f"-{tag}", '')
             str = str.replace(f"-{tag}-edge", '')
-        for vtag in version_specific_tags:
-            for version in versions:
-                str = str.replace(vtag.format(version), '')
-                str = str.replace(vtag.format(version) + '-edge', '')
+
+        validator_regex = re.compile(r"^\d+\.\d+\.\d+-\d+$")
+        if not validator_regex.match(str):
+            raise ExceptionWithLog(f"The stripped release did not yield a valid version! ({str})")
+
         return str
 
     def maybe_download_vmlinux_ddeb(self):
@@ -335,29 +394,31 @@ class hotkdump:
             self.get_architecture()
         )
 
-        # Check if we already have the .ddeb
-        if os.path.exists(expected_ddeb_path):
-            # Already exists, do not download again
-            # TODO(mkg): Verify SHA checksum?
+        with self.switch_cwd(self.ddebs_path):
+            # Check if we already have the .ddeb
+            if os.path.exists(expected_ddeb_path):
+                # Already exists, do not download again
+                # TODO(mkg): Verify SHA checksum?
+                logging.info(
+                    f"The .ddeb file {expected_ddeb_path} already exists, re-using it")
+                # Ensure that the file's last access time is updated
+                os.utime(expected_ddeb_path, (time.time(), time.time()))
+                return expected_ddeb_path
+
             logging.info(
-                f"The .ddeb file {expected_ddeb_path} already exists, re-using it")
-            return expected_ddeb_path
+                f"Downloading `vmlinux` image for kernel version {self.kdump_header.release}, please be patient...")
 
-        logging.info(
-            f"Downloading `vmlinux` image for kernel version {self.kdump_header.release}, please be patient...")
+            # (mkg): To force pull-lp-ddebs to use launchpadlibrarian.net for download
+            # pass an empty mirror list env variable to the hotkdump, e.g.:
+            # UBUNTUTOOLS_UBUNTU_DDEBS_MIRROR= python3 hotkdump.py -c 123 -d dump.dump
+            pull_args = ["--distro", "ubuntu", "--arch", self.get_architecture(), "--pull", "ddebs",
+                        f"linux-image-unsigned-{self.kdump_header.release}",
+                        f"{self.strip_release_variant_tags(self.kdump_header.release)}.{self.kdump_header.normalized_version}"]
+            logging.info(f"Invoking PullPkg().pull with {str(pull_args)}")
 
-        # (mkg): To force pull-lp-ddebs to use launchpadlibrarian.net for download
-        # pass an empty mirror list env variable to the hotkdump, e.g.:
-        # UBUNTUTOOLS_UBUNTU_DDEBS_MIRROR= python3 hotkdump.py -c 123 -d dump.dump
-        pull_args = ["--distro", "ubuntu", "--arch", self.get_architecture(), "--pull", "ddebs",
-                     f"linux-image-unsigned-{self.kdump_header.release}",
-                     f"{self.strip_release_variant_tags(self.kdump_header.release)}.{self.kdump_header.normalized_version}"]
-        logging.info(f"Invoking PullPkg().pull with {str(pull_args)}")
-
-        PullPkg().pull(pull_args)
-
-        if not os.path.exists(expected_ddeb_path):
-            raise ExceptionWithLog(f"failed to download {expected_ddeb_path}")
+            PullPkg().pull(pull_args)
+            if not os.path.exists(expected_ddeb_path):
+                raise ExceptionWithLog(f"failed to download {expected_ddeb_path}")
 
         return expected_ddeb_path
 
@@ -372,8 +433,9 @@ class hotkdump:
         """
         ddeb_extract_dst = f"{self.temp_working_dir.name}/ddeb-root"
         dpkg_deb_args = f"-x {ddeb_file} {ddeb_extract_dst}"
-        logging.debug(f"extracting {ddeb_file} to {ddeb_extract_dst}")
-        result = self.exec("dpkg", dpkg_deb_args)
+        logging.info(f"Extracting {ddeb_file} to {ddeb_extract_dst}, please be patient...")
+        with self.switch_cwd(self.ddebs_path):
+            result = self.exec("dpkg", dpkg_deb_args)
         if not (result.returncode == 0):
             raise ExceptionWithLog(
                 f"failed to extract {ddeb_file}: {result.stderr.readlines()}")
@@ -387,10 +449,10 @@ class hotkdump:
         """
         logging.info(
             f"Loading `vmcore` file {self.vmcore_file} into `crash`, please wait..")
+
         self.exec(self.crash_executable,
                   f"-i {self.commands_file_path} -s {self.vmcore_file} {self.vmlinux_path}")
-        logging.info("See hotkdump.log for logs")
-        logging.info("See hotkdump.out for outputs")
+        logging.info(f"See {self.log_file} for logs, {self.output_file} for outputs")
 
     def launch_crash(self):
         """Launch the `crash` application with the user-given vmcore and 
@@ -400,6 +462,64 @@ class hotkdump:
             f"Loading `vmcore` file {self.vmcore_file} into `crash`, please wait..")
         self.exec(self.crash_executable,
                   f"{self.vmcore_file} {self.vmlinux_path}")
+
+    def post_run(self):
+        """Perform post-run tasks
+        """
+        # Retrieve all ddebs and their stat() from the ddeb folder
+        ddebs = [(f"{self.ddebs_path}/{file}", os.stat(f"{self.ddebs_path}/{file}")) for file in os.listdir(self.ddebs_path) if file.endswith(".ddeb")]
+        # Sort ddebs by their last access time
+        ddebs.sort(key=lambda f: f[1].st_atime, reverse=True)
+
+
+        # TODO(mkg): Wrap this logic into its own class
+        # i.e. RetentionPolicy
+
+        def remove_ddeb(ddeb_info, reason):
+            (ddeb_path, ddeb_stat) = ddeb_info
+            os.remove(ddeb_path)
+            logging.debug(f"removed {ddeb_path} to reclaim {pretty_size(ddeb_stat.st_size)}. age: {(time.time() - ddeb_stat.st_atime):.2f} seconds. reason: {reason}")
+
+        if not self.ddeb_retention_enabled:
+            logging.debug(f"ddeb retention is disabled, starting cleanup.")
+            for ddeb_info in ddebs:
+                remove_ddeb(ddeb_info, "retention disabled")
+            return
+
+        # Prune based on count
+        if self.ddeb_retention_max_ddeb_count and len(ddebs) > self.ddeb_retention_max_ddeb_count:
+            ddebs_to_remove = ddebs[self.ddeb_retention_max_ddeb_count:]
+
+            for ddeb_info in ddebs_to_remove:
+                remove_ddeb(ddeb_info, "count")
+
+            # Update the list
+            ddebs = list(filter(lambda i: i not in ddebs_to_remove, ddebs))
+
+        # Prune based on age
+        if self.ddeb_retention_max_age_secs:
+            ddebs_to_remove = [v for v in ddebs if (time.time() - v[1].st_atime) >= self.ddeb_retention_max_age_secs]
+            for ddeb_info in ddebs_to_remove:
+                remove_ddeb(ddeb_info, "old age")
+
+            # Update the list
+            ddebs = list(filter(lambda i: i not in ddebs_to_remove, ddebs))
+
+        if self.ddeb_retention_size_low_wm_bytes and self.ddeb_retention_size_high_wm_bytes:
+            # Check if size of all ddebs has reached to the high watermark
+            total_ddeb_size = sum([ddeb_info[1].st_size for ddeb_info in ddebs])
+            if total_ddeb_size >= self.ddeb_retention_size_high_wm_bytes:
+                logging.debug(f"total ddeb size of {pretty_size(total_ddeb_size)} exceeds the high watermark {pretty_size(self.ddeb_retention_size_high_wm_bytes)}, starting cleanup.")
+                # Remove .ddebs until total size is below the low watermark
+                while len(ddebs) > 0:
+                    total_ddeb_size = sum([ddeb_info[1].st_size for ddeb_info in ddebs])
+                    if total_ddeb_size < self.ddeb_retention_size_low_wm_bytes:
+                        logging.debug(f"total ddeb folder size is now below {pretty_size(self.ddeb_retention_size_low_wm_bytes)} low watermark, stopping cleanup.")
+                        break
+                    ddeb_info = ddebs.pop()
+                    remove_ddeb(ddeb_info, "size")
+
+        logging.debug(f"postrun-aftermath: ddeb folder final size {pretty_size(sum([ddeb_info[1].st_size for ddeb_info in ddebs]))}, {len(ddebs)} cached ddebs remain.")
 
 
 def main():
@@ -419,20 +539,29 @@ def main():
                     default=default_output_file)
     ap.add_argument("-l", "--log-file",
                     help="log file path", default=default_log_file)
+    ap.add_argument("-p", "--ddebs-path",
+                    help="ddebs path", default=default_ddebs_path)
     args = vars(ap.parse_args())
+
     hkd = hotkdump(args['casenum'], args['dump'],
-                   args['output_path'], args['log_file'])
-    vmlinux_ddeb = hkd.maybe_download_vmlinux_ddeb()
-    if vmlinux_ddeb == "":
-        print("got empty vmlinux")
-        return
+                   args['output_path'], args['log_file'],
+                args['ddebs_path'])
 
-    hkd.extract_vmlinux_ddeb(vmlinux_ddeb)
+    try:
 
-    if args['interactive']:
-        hkd.launch_crash()
-    else:
-        hkd.summarize_vmcore_file()
+        vmlinux_ddeb = hkd.maybe_download_vmlinux_ddeb()
+        if vmlinux_ddeb == "":
+            print("got empty vmlinux")
+            return
+
+        hkd.extract_vmlinux_ddeb(vmlinux_ddeb)
+
+        if args['interactive']:
+            hkd.launch_crash()
+        else:
+            hkd.summarize_vmcore_file()
+    finally:
+        hkd.post_run()
 
     diff = time.time() - start
     print(f"hotkdump took {round(diff, 2)} secs")
